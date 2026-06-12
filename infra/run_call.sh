2cfbe1d691d4e55e1d21731f1b7e35bf18c2281f
A5HW1NRA7AN <rajanashwin6@gmail.com>
2026-06-12 10:29:44 +0000

    Update test scripts for Kubernetes deployment

--- infra/run_call.sh
+++ infra/run_call.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ./freeswitch-key.pem -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i ./freeswitch-key.pem -o StrictHostKeyChecking=no -W %h:%p admin@52.69.97.143" admin@10.0.1.143 'docker exec telephony-freeswitch fs_cli -p CluSt3r@Esl#2026! -x "originate loopback/1000/public &playback(/usr/share/freeswitch/sounds/custom/greeting.wav)"'
+ssh -i ./freeswitch-key.pem -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i ./freeswitch-key.pem -o StrictHostKeyChecking=no -W %h:%p ubuntu@18.183.139.53" ubuntu@10.0.1.143 'kubectl exec -n default deployment/freeswitch -- fs_cli -p CluSt3r@Esl#2026! -x "originate loopback/1000/public &playback(/usr/share/freeswitch/sounds/custom/greeting.wav)"'
